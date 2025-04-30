94a5e5700100db6f5d88ff5a629430932d85aea9
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-04-30 10:11:32 +0700

    feat : create DataLoader class cpp

---
+++ cpp/src/DataLoader.cpp
@@ -0,0 +1,42 @@
+#include "include/DataLoader.h"
+
+DataLoader::DataLoader(std::string config_file){
+    YAML::Node config = YAML::LoadFile(config_file);
+    
+    modelPath = config["model_path"].as<std::string>();
+    datasetDir = config["dataset_path"].as<std::string>();
+    outputDir = config["output_dir"].as<std::string>();
+
+    for(const auto& data : config["model_config"]){
+        const auto& value = data.second;
+
+        std::string label = value["label"].as<std::string>();
+        
+        classRemap.insert({
+                            data.first.as<int>(), 
+                            value["remap"].as<int>()
+                        });
+
+        classes.push_back(label);
+    }
+}
+
+std::string DataLoader::GetModelPath(){
+    return modelPath;
+}
+
+std::string DataLoader::GetDatasetPath(){
+    return datasetDir;
+}
+
+std::string DataLoader::GetOutputPath(){
+    return outputDir;
+}
+
+std::vector<std::string> DataLoader::GetModelClasses(){
+    return classes;
+}
+
+std::map<int, int> DataLoader::GetClassRemap(){
+    return classRemap;
+}
