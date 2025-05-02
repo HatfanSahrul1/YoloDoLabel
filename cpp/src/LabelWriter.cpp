d2295bb9f7e05fc1251453b45215357de0c29a14
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-05-03 01:12:33 +0700

    style : change variables name

--- cpp/src/LabelWriter.cpp
+++ cpp/src/LabelWriter.cpp
@@ -1,32 +1,29 @@
 #include "include/LabelWriter.h"
 
-void LabelWriter::write(const std::string& imagePath,
-                        const std::vector<Detection>& detections,
-                        const cv::Size& imageSize,
-                        const LabelMapper& mapper,
+void LabelWriter::write(const std::string& image_name, 
+                        const std::vector<Detection>& detections, 
+                        const cv::Size& img_size, 
+                        const LabelMapper& mapper, 
                         const std::string& outputDir){
 
-    // Ambil nama file tanpa ekstensi
-    fs::path inputPath(imagePath);
-    std::string baseName = inputPath.stem().string(); // contoh: "image_001"
-
-    // Buat path output
+    fs::path inputPath(image_name);
+    std::string baseName = inputPath.stem().string() + ".txt";
+    
     fs::create_directories(outputDir);
-    std::string outPath = outputDir + "/" + baseName + ".txt";
+    fs::path outPath = fs::path(outputDir) / baseName;
 
     std::ofstream file(outPath);
     if (!file.is_open()) {
-        throw std::runtime_error("Gagal membuka file untuk menulis: " + outPath);
+        throw std::runtime_error("Gagal membuka file untuk menulis: " + outPath.string());
     }
 
     for (const auto& det : detections) {
         int remappedID = mapper.map(det.class_id);
 
-        // Normalisasi koordinat bounding box
-        float x_center = (det.box.x + det.box.width / 2.0f) / imageSize.width;
-        float y_center = (det.box.y + det.box.height / 2.0f) / imageSize.height;
-        float width = det.box.width / static_cast<float>(imageSize.width);
-        float height = det.box.height / static_cast<float>(imageSize.height);
+        float x_center = (det.box.x + det.box.width / 2.0f) / img_size.width;
+        float y_center = (det.box.y + det.box.height / 2.0f) / img_size.height;
+        float width = det.box.width / static_cast<float>(img_size.width);
+        float height = det.box.height / static_cast<float>(img_size.height);
 
         file << remappedID << " "
         << x_center << " "
