449e56212b3301f736264c3d6d741a259cbcd1c9
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-05-02 22:37:32 +0700

    feat : update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,23 +2,14 @@
 #include <string>
 #include <map>
 
-// #include "YoloAnnotator.h"
+#include "include/YoloAnnotator.h"
 #include "include/DataLoader.h"
 
 int main() {
     DataLoader dataLoader("/home/eros/clone/yoloDoLabel/cpp/config.yaml");
-    // std::string modelPath = "model.onnx";
-    // std::string classFile = "model_classes.txt";
-    // std::string datasetDir = "dataset/images";
-    // std::string outputDir = "dataset/labels";
+    YoloAnnotator yoloAnnotator(dataLoader);
 
-    // std::map<int, int> classRemap = {
-    //     {0, 2}, // bola dari model (id 0) disimpan sebagai id 2
-    //     {1, 0}, // robot biru → id 0
-    //     {2, 1}  // robot merah → id 1
-    // };
-
-    // YoloAnnotator annotator(modelPath, classFile, datasetDir, outputDir, classRemap);
-    // annotator.run();
+    yoloAnnotator.Run();
+    
     return 0;
 }
